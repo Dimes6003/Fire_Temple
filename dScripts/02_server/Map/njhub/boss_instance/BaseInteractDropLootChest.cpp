45d71fc283e6ff49af921dc889bcb87c46ede0c3
Dimes6003 <166362238+Dimes6003@users.noreply.github.com>
2024-08-09 06:55:40 -0700

    Add files via upload

--- dScripts/02_server/Map/njhub/boss_instance/BaseInteractDropLootChest.cpp
+++ dScripts/02_server/Map/njhub/boss_instance/BaseInteractDropLootChest.cpp
@@ -3,28 +3,22 @@
 #include "GameMessages.h"
 
 void BaseInteractDropLootChest::OnUse(Entity* self, Entity* user) {
-	BaseUse(self, user);
+	if (!self->GetNetworkVar<bool>(u"bInUse")) {
+		BaseUse(self, user);
+	}
 }
 
 void BaseInteractDropLootChest::BaseUse(Entity* self, Entity* user) {
-	auto cooldownTime = self->GetVar<float>(u"cooldownTime");
-	if (cooldownTime == 0) cooldownTime = 59893;
-
 	uint32_t lootMatrix = self->GetVar<int32_t>(u"UseLootMatrix");
 	if (lootMatrix == 0) lootMatrix = self->GetVar<int32_t>(u"smashable_loot_matrix");
 	if (lootMatrix == 0) lootMatrix = 919;
 
 	auto useSound = self->GetVar<std::string>(u"sound1");
-
 	if (!useSound.empty()) {
 		GameMessages::SendPlayNDAudioEmitter(self, user->GetSystemAddress(), useSound);
 	}
 
-	self->SetNetworkVar(u"bInUse", true);
+	self->SetNetworkVar<bool>(u"bInUse", true);
 
 	Loot::DropLoot(user, self, lootMatrix, 0, 0);
-
-	self->AddCallbackTimer(cooldownTime, [this, self]() {
-		self->SetNetworkVar(u"bInUse", false);
-		});
 }
